a3cb51a69416c2e0e6084b8dee3754e573adc4e5
Hardik Prabhu <hardik.prabhu@gmail.com>
2024-02-07 01:31:38 +0530

    Updated modelling pipeline

--- folds-cv.py
+++ folds-cv.py
@@ -1,12 +1,12 @@
 import pickle
 from main import create_model_data
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import time
 import itertools
-from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
 from evaluation import classification_report
+from models import Model
+
 
 def get_combinations(lst):
     combinations = []
@@ -22,7 +22,7 @@ eval_mcc = True
 eval_counts = True
 model = None
 # Select -> random forest : "rf", support vector machine : "svm", logistic regression : "linear", Neural net : "ann"
-model_name = "rf"
+model_name = "ann"
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -33,43 +33,41 @@ if __name__ == "__main__":
     r_df = pd.DataFrame()
     for includes in get_combinations(["conform", "no_reduction", "hydro"]):
         for types in get_combinations(["counts", "mat", "tfeat", "protvec"]):
-                for offsets in get_combinations([1, 2, 3]):  # [1,2,3]
-                    print(includes, types, offsets)
-                    if types in get_combinations(["counts", "protvec"]) and offsets != tuple(
-                            [1]):  # redundant combinations
-                        break
-                    data, y_data = create_model_data(df, include=includes, type=types, offsets=offsets,
-                                                     is_train=None)
-                    kf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=1)
-                    evaluation = {}  # a dict to maintain results
-                    for train_index_vals, test_index_vals in kf.split(data, y_data):
-                        train_X = data[train_index_vals]
-                        train_y = y_data.iloc[train_index_vals]
-                        test_X = data[test_index_vals]
-                        test_y = y_data.iloc[test_index_vals]
-                        feature_len = train_X.shape[-1]
-                        start_time = time.time()
-                        if model_name == "rf":
-                            model = RandomForestClassifier(random_state=42)
-                        model.fit(train_X, train_y)
-                        pred = model.predict(test_X)
-                        prob = model.predict_proba(test_X)
-                        prob = prob[:, 1]
-                        # Calculate metrics
-                        # Generate classification report
-                        report = classification_report(test_y, pred, prob)
-                        for key in report:
-                            evaluation[key] = evaluation.get(key, 0) + report[key]
+            for offsets in get_combinations([1, 2, 3]):  # [1,2,3]
+                print(includes, types, offsets)
+                if types in get_combinations(["counts", "protvec"]) and offsets != tuple(
+                        [1]):  # redundant combinations
+                    break
+                data, y_data = create_model_data(df, include=includes, type=types, offsets=offsets,
+                                                 is_train=None)
+                kf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=1)
+                evaluation = {}  # a dict to maintain results
+                for train_index_vals, test_index_vals in kf.split(data, y_data):
+                    train_X = data[train_index_vals]
+                    train_y = y_data.iloc[train_index_vals]
+                    test_X = data[test_index_vals]
+                    test_y = y_data.iloc[test_index_vals]
+                    feature_len = train_X.shape[-1]
+                    start_time = time.time()
+                    model = Model(model_name, input_dim=train_X.shape[1])
+                    model.fit(train_X, train_y)
+                    pred = model.predict(test_X)
+                    prob = model.predict_proba(test_X)
+                    # Calculate metrics
+                    # Generate classification report
+                    report = classification_report(test_y, pred, prob)
+                    for key in report:
+                        evaluation[key] = evaluation.get(key, 0) + report[key]
 
-                    for key in evaluation:
-                        evaluation[key] = evaluation.get(key) / folds
-                    print(evaluation)
+                for key in evaluation:
+                    evaluation[key] = evaluation.get(key) / folds
+                print(evaluation)
 
-                    new_entry = {"model": model_name, "string_conversions": includes, "feat_types": types,
-                                 "dataset": f"{folds}-folds cv", "offsets": offsets, "nfeat": feature_len}
-                    new_entry.update(evaluation)
+                new_entry = {"model": model_name, "string_conversions": includes, "feat_types": types,
+                             "dataset": f"{folds}-folds cv", "offsets": offsets, "nfeat": feature_len}
+                new_entry.update(evaluation)
 
-                    r_df = r_df._append(new_entry, ignore_index=True)
+                r_df = r_df._append(new_entry, ignore_index=True)
 
     r_df = r_df.sort_values(['f1_score', 'nfeat'], ascending=[False, True])
 
---
+++ models.py
@@ -0,0 +1,122 @@
+import torch
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+"""
+Model options
+--------------
+  Models that can be used, model_name to be used while constructing Model object
+  - Random Forest : "rf"
+  - support vector machine : "svm"
+  - Logistic regression : "linear"
+  - Neural net : "ann"
+  - Xgboost : "xgboost"
+  - LightGBM : "lgbm"
+
+"""
+
+
+class MyNet(nn.Module):
+    def __init__(self, input_dim):
+        super(MyNet, self).__init__()
+        self.fc1 = nn.Linear(input_dim, input_dim)
+        self.fc2 = nn.Linear(input_dim, max(10, input_dim // 2))
+        self.fc3 = nn.Linear(max(10, input_dim // 2), 1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.sigmoid(x)
+        return x
+
+    def fit(self, X_train, Y_train, epochs=100, learning_rate=0.01, batch_size=32):
+        """
+        Trains the neural network using the provided training data and labels.
+
+        Parameters:
+            X_train (Tensor): The training data features.
+            Y_train (Tensor): The training data labels.
+            epochs (int): The number of times to iterate over the entire dataset.
+            learning_rate (float): The learning rate for the optimizer.
+            batch_size (int): The size of each batch to be used during training.
+        """
+        criterion = nn.BCELoss()
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        dataset_size = X_train.shape[0]
+        num_batches = (dataset_size + batch_size - 1) // batch_size  # Calculate the number of batches
+
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i in range(num_batches):
+                start = i * batch_size
+                end = start + batch_size
+                inputs = X_train[start:end]
+                labels = Y_train[start:end]
+                optimizer.zero_grad()
+                outputs = self(inputs)
+                loss = criterion(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            print(f"Epoch {epoch + 1}, Loss: {running_loss / num_batches}")
+
+    def predict(self, X_batch):
+        with torch.no_grad():
+            return self(X_batch).round()
+
+    def predict_proba(self, X_batch):
+        with torch.no_grad():
+            return self(X_batch)
+
+
+class Model():
+    """
+    A wrapper class for machine learning models that simplifies the process of fitting,
+    predicting, and obtaining prediction probabilities.
+    Attributes:
+        model_name (str): The name of the model to use.
+        kwargs: key word arguments specific to the model.
+    """
+
+    def __init__(self, model_name="rf", **kwargs):
+        self.model_name = model_name
+        if self.model_name == "rf":
+            self.model = RandomForestClassifier(**kwargs)
+        if self.model_name == "svm":
+            self.model = SVC(probability=True, **kwargs)
+        if self.model_name == "linear":
+            self.model = LogisticRegression(**kwargs)
+        if self.model_name == "ann":
+            self.model = MyNet(**kwargs)
+
+    def fit(self, Xtrain, Ytrain, **kwargs):
+        if self.model_name == "ann":
+            Xtrain = torch.tensor(Xtrain).float()
+            Ytrain = torch.tensor(np.array(Ytrain)).float().view(-1, 1)
+        self.model.fit(Xtrain, Ytrain)
+
+    def predict(self, X):
+        if self.model_name == "ann":
+            X = torch.tensor(X).float()
+        return self.model.predict(X)
+
+    def predict_proba(self, Xtest):
+
+        if self.model_name == "ann":
+            Xtest = torch.tensor(Xtest).float()
+        if self.model_name in ["rf", "svm", "linear"]:
+            prob = self.model.predict_proba(Xtest)
+            prob = prob[:, 1]
+            return prob
+        return self.model.predict_proba(Xtest)
